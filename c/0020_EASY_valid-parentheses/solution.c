aa25c108528db9c9b59aa734bd334903ad550a90
Moahmed Dhia Sleimi <sleimi.mohameddhia@gmail.com>
2024-06-25 22:18:05 +0100

    solved problem N°20 in c

---
+++ c/0020_EASY_valid-parentheses/solution.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char *s) {
+  int len = strlen(s);
+  char stack[len];
+  int top = -1;
+
+  for (int i = 0; i < len; i++) {
+    if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+      stack[++top] = s[i];
+    } else {
+      if (top == -1)
+        return false;
+      if (s[i] == ')' && stack[top] != '(')
+        return false;
+      if (s[i] == '}' && stack[top] != '{')
+        return false;
+      if (s[i] == ']' && stack[top] != '[')
+        return false;
+      top--;
+    }
+  }
+  return top == -1;
+}
