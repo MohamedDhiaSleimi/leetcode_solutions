a4d30b10ca3026de84cda7a69fbe409d2d15fd11
Moahmed Dhia Sleimi <sleimi.mohameddhia@gmail.com>
2024-06-25 21:05:31 +0100

    solved problem N°1038

---
+++ 1038_MEDM_binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val: int = val
+        self.left: TreeNode = left
+        self.right: TreeNode = right
+
+
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        def reverse_in_order_traversal(node, acc_sum):
+            if not node:
+                return acc_sum
+            acc_sum: int = reverse_in_order_traversal(node.right, acc_sum)
+            node.val += acc_sum
+            acc_sum = node.val
+            acc_sum = reverse_in_order_traversal(node.left, acc_sum)
+            return acc_sum
+
+        reverse_in_order_traversal(root, 0)
+        return root
